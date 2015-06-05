1940409cce8690905a2b101e71ed226b80158094
Dima <Dima_Vovchuk@inbox.ru>
2015-06-06 01:13:24 +0300

    Try connect google maps api

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,8 +1,15 @@
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.model.GeocodingResult;
+
 /**
  * Created by Dima on 05-Jun-15.
  */
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("hello");
+    public static void main(String[] args) throws Exception {
+        // Replace the API key below with a valid API key.
+        GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyCL9BI_9U8ba_Zf_ldHd9KrYFtBtK7cTzI");
+        GeocodingResult[] results = GeocodingApi.geocode(context,"Lviv").await();
+        System.out.println(results[0].formattedAddress);
     }
 }
