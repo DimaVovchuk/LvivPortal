709bb11b50ed2708f2f7df173190ed961c93928d
Oleguk <oleguk.simp@gmail.com>
2015-06-09 02:41:10 +0300

    User model created

---
+++ src/main/java/com/epam/lab/model/User.java
@@ -0,0 +1,138 @@
+package com.epam.lab.model;
+
+/**
+ * Created by Oleguk on 09.06.2015.
+ */
+public class User {
+
+    private Integer id;
+    private Integer rating;
+    private String name;
+    private String surname;
+    private String login;
+    private String mail;
+    private String password;
+    private String phone;
+    private String status;
+    private String role;
+    private Boolean deleted;
+
+    public User(Integer id, Integer rating, String name, String surname, String login, String mail, String password, String phone, String status, String role, Boolean deleted) {
+        this.id = id;
+        this.rating = rating;
+        this.name = name;
+        this.surname = surname;
+        this.login = login;
+        this.mail = mail;
+        this.password = password;
+        this.phone = phone;
+        this.status = status;
+        this.role = role;
+        this.deleted = deleted;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", rating=" + rating +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", login='" + login + '\'' +
+                ", mail='" + mail + '\'' +
+                ", password='" + password + '\'' +
+                ", phone='" + phone + '\'' +
+                ", status='" + status + '\'' +
+                ", role='" + role + '\'' +
+                ", deleted=" + deleted +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+}
