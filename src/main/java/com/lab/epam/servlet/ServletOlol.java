7c55a2f57d737a52963b7011f702e2c0dbadee9f
niceapple91 <olya.podgornaya@gmail.com>
2015-06-08 14:56:09 +0300

    New Serwlet

---
+++ src/main/java/com/lab/epam/servlet/ServletOlol.java
@@ -0,0 +1,20 @@
+package com.lab.epam.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Admin on 08.06.2015.
+ */
+public class ServletOlol extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
