779f640d6198b036d4f1b038b93f0c2a114e2aeb
niceapple91 <olya.podgornaya@gmail.com>
2015-06-09 12:53:26 +0300

    Commit with time proba. Commit is not ended.

--- src/main/java/com/lab/epam/servlet/TryTimeServlet.java
+++ src/main/java/com/lab/epam/servlet/TryTimeServlet.java
@@ -4,6 +4,7 @@ import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.model.GeocodingResult;
 import com.google.maps.model.LatLng;
+import netscape.javascript.JSObject;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,29 +13,29 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * Created by Dima on 07-Jun-15.
+ * Created by Admin on 08.06.2015.
  */
-public class TryMapServlet extends HttpServlet {
+public class TryTimeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyCL9BI_9U8ba_Zf_ldHd9KrYFtBtK7cTzI");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyCL9BI_9U8ba_Zf_ldHd9KrYFtBtK7cTzI");
         GeocodingResult[] resultsFrom = new GeocodingResult[0];
-        GeocodingResult[] resultsTo = new GeocodingResult[0];
-        System.out.println("Ololol");
         try {
             resultsFrom = GeocodingApi.geocode(context, "Lviv, Svobody Prospect").await();
-            resultsTo = GeocodingApi.geocode(context, "Stryi").await();
         } catch (Exception e) {
             e.printStackTrace();
         }
+      //  JSObject win = JSObject.getWindow(this);
+
+
         LatLng southwestFrom = resultsFrom[0].geometry.bounds.southwest;
-        LatLng southwestTo = resultsTo[0].geometry.bounds.southwest;
         request.setAttribute("from_x", southwestFrom.lat);
         request.setAttribute("from_y", southwestFrom.lng);
-        request.setAttribute("to_x",southwestTo.lat);
-        request.setAttribute("to_y",southwestTo.lng);
-        request.getRequestDispatcher("views/map.jsp").forward(request,
+        request.setAttribute("time", 0);
+        request.getRequestDispatcher("views/triptime.jsp").forward(request,
                 response);
     }
 }
