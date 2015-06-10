2339beaaca1ebe615a339d0fb9f65d4f16acab72
Dima <Dima_Vovchuk@inbox.ru>
2015-06-10 18:40:39 +0300

    Переробив логер

--- src/main/java/com/lab/epam/listener/Log4jInit.java
+++ src/main/java/com/lab/epam/listener/Log4jInit.java
@@ -3,19 +3,22 @@ package com.lab.epam.listener;
 /**
  * Created by Vasyl on 10.06.2015.
  */
+
 import org.apache.log4j.PropertyConfigurator;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 import java.io.File;
 
+@WebListener("application context listener")
 public class Log4jInit implements ServletContextListener {
     public void contextInitialized(ServletContextEvent event) {
-        String homeDir = event.getServletContext().getRealPath("/");
-        File propertiesFile = new File(homeDir,
-                "./resources/log4j/log4j.properties");
-        System.out.println(propertiesFile.toString());
-        PropertyConfigurator.configure(propertiesFile.toString());
+        ServletContext context = event.getServletContext();
+        String log4jConfigFile = context.getInitParameter("log4j-config-location");
+        String fullPath = context.getRealPath("") + File.separator + log4jConfigFile;
+        PropertyConfigurator.configure(fullPath.toString());
     }
 
     public void contextDestroyed(ServletContextEvent event) {
