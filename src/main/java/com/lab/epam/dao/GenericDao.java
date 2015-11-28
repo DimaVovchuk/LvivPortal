90db672ffc6a0349bd8d4da23285e3ad8a629c55
Dima_Vovchuk@inbox.ru <Dima64838912>
2015-11-28 17:18:23 +0200

    delete comment

--- src/main/java/com/lab/epam/dao/GenericDao.java
+++ src/main/java/com/lab/epam/dao/GenericDao.java
@@ -8,21 +8,15 @@ import java.util.List;
  */
 public interface GenericDao<T extends Identified<PK>, PK extends Serializable> {
 
-    /** Создает новую запись и соответствующий ей объект */
     public void create(T object) throws PersistException;
 
-    /** Возвращает объект соответствующий записи с первичным ключом key или null */
     public T getByPK(PK key) throws PersistException;
 
-    /** Сохраняет состояние объекта group в базе данных */
     public void update(T object) throws PersistException;
 
-    /** Удаляет запись об объекте из базы данных */
     public void delete(T object) throws PersistException;
 
-    /** Возвращает список объектов соответствующих всем записям в базе данных */
     public List<T> getAll() throws PersistException;
 
-    /** Возвращает список объектов соответствующих всем записям в базе данных даже удленных */
     public List<T> getAllWithoutDeleted() throws PersistException;
 }
